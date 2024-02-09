1a25982e4f29e15004715aabc109be5640efe63e
Yume <hei.hajatiana@gmail.com>
2024-02-09 22:42:04 +0300

    chore: add post provider

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,8 @@
 // https://github.com/blogify-app/blogify-web/blob/preprod/src/services/api/index.ts
-import {SecurityApi} from "@/services/api/gen";
+import {SecurityApi, PostingApi} from "@/services/api/gen";
 import {AuthProvider} from "@/services/security";
 
 export const securityApi = () => new SecurityApi(AuthProvider.getAuthConf());
+export const postingApi = () => new PostingApi(AuthProvider.getAuthConf());
 
 export * from "./provider";
--- src/services/api/provider/index.ts
+++ src/services/api/provider/index.ts
@@ -1,3 +1,4 @@
 export * from "./data_provider.ts";
 export * from "./health_provider.ts";
 export * from "./security_provider.ts";
+export * from "./post_provider.ts";
