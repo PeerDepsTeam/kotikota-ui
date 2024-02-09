4ffde1f2a0d85f66c511e8bdff7b349ef322e139
Ylan <hei.nalisoa@gmail.com>
2024-02-10 01:44:15 +0300

    feat: signup page

--- src/pages/auth/SignupPage.tsx
+++ src/pages/auth/SignupPage.tsx
@@ -1,13 +1,16 @@
 import {FC} from "react";
 import {Layout, NavBar} from "@/layout";
 import {Stepper} from "@/components/common/stepper.tsx";
+import { Signup } from "@/features/auth/components/SignUp";
 
 export const SignupPage: FC = () => {
   // it may also have some layout-ing but at the end, It'll always use its root from the corresponding feature
   return (
     <Layout header={<NavBar page="sign_up" />}>
       <Stepper>
-        <div className="mx-[2.8rem] h-full pt-[3.8rem] md:mx-[11rem]"></div>
+        <div className="mx-[2.8rem] h-full pt-[3.8rem] md:mx-[11rem]">
+          <Signup/>
+        </div>
       </Stepper>
     </Layout>
   );
