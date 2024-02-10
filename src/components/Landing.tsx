96985f298b9a0b7e948d5670abfbd42845977d15
Karenathi <hei.karenandriantasy@gmail.com>
2024-02-10 07:58:10 +0300

    fix: resolving some bugs

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -38,10 +38,9 @@ export const Landing: FC = () => {
   );
 };
 
-function PlayCircleIcon(props) {
+function PlayCircleIcon({className}:{className:string}) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -51,6 +50,7 @@ function PlayCircleIcon(props) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      className={className}
     >
       <circle cx="12" cy="12" r="10" />
       <polygon points="10 8 16 12 10 16 10 8" />
