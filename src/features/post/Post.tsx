57a7fda1d7b063b3cf7ddbfa2308163315bcd0a6
Karenathi <hei.karenandriantasy@gmail.com>
2024-02-10 07:14:24 +0300

    fix: code style

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -4,16 +4,16 @@ import {Layout} from "@/layout";
 import {Icons} from "@/components/common/icons";
 import {Button} from "@/components/common/button";
 import pic from "@/assets/profile.jpg";
-import { FacebookShareButton, TwitterShareButton } from "react-share";
-import { Post } from "@/services/api/gen";
-import { Reader } from "../wisiwig";
+import {FacebookShareButton, TwitterShareButton} from "react-share";
+import {Post} from "@/services/api/gen";
+import {Reader} from "../wisiwig";
 
 export interface PostProps {
   post: Post | undefined;
 }
 
-export const PostCard: FC<PostProps> = ({ post }) => {
-  const url = "kotikota.vercel.app/posts/"+post?.id
+export const PostCard: FC<PostProps> = ({post}) => {
+  const url = "kotikota.vercel.app/posts/" + post?.id;
   return (
     <Layout>
       <div
@@ -64,9 +64,9 @@ export const PostCard: FC<PostProps> = ({ post }) => {
                 className=" m-auto mb-[5rem] h-[40rem] w-[70rem]"
               />
             </div>
-              <div className="mx-10">
-                <Reader>{post?.content!}</Reader>
-              </div>
+            <div className="mx-10">
+              <Reader>{post?.content!}</Reader>
+            </div>
             <div className=" flex justify-between">
               <div className="flex items-stretch justify-self-center">
                 <Button
@@ -93,20 +93,40 @@ export const PostCard: FC<PostProps> = ({ post }) => {
               <div className="flex justify-between ">
                 <Button>Donate</Button>
                 <span className="flex justify-between">
-                 <div className="mx-2 rounded-[8px] p-2 flex items-center">
-                      <FacebookShareButton  url={url}>
-                        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                          <path fill-rule="evenodd" d="M13.1 6H15V3h-1.9A4.1 4.1 0 0 0 9 7.1V9H7v3h2v10h3V12h2l.6-3H12V6.6a.6.6 0 0 1 .6-.6h.5Z" clip-rule="evenodd"/>
-                        </svg>
-                      </FacebookShareButton>
-                 </div>
-                 <div className="rounded-[8px] p-2 flex items-center">
-                  <TwitterShareButton  url={url}>
-                      <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" d="M22 5.9c-.7.3-1.5.5-2.4.6a4 4 0 0 0 1.8-2.2c-.8.5-1.6.8-2.6 1a4.1 4.1 0 0 0-6.7 1.2 4 4 0 0 0-.2 2.5 11.7 11.7 0 0 1-8.5-4.3 4 4 0 0 0 1.3 5.4c-.7 0-1.3-.2-1.9-.5a4 4 0 0 0 3.3 4 4.2 4.2 0 0 1-1.9.1 4.1 4.1 0 0 0 3.9 2.8c-1.8 1.3-4 2-6.1 1.7a11.7 11.7 0 0 0 10.7 1A11.5 11.5 0 0 0 20 8.5V8a10 10 0 0 0 2-2.1Z" clip-rule="evenodd"/>
-                      </svg>  
+                  <div className="mx-2 flex items-center rounded-[8px] p-2">
+                    <FacebookShareButton url={url}>
+                      <svg
+                        className="h-6 w-6 text-gray-800 dark:text-white"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M13.1 6H15V3h-1.9A4.1 4.1 0 0 0 9 7.1V9H7v3h2v10h3V12h2l.6-3H12V6.6a.6.6 0 0 1 .6-.6h.5Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </FacebookShareButton>
+                  </div>
+                  <div className="flex items-center rounded-[8px] p-2">
+                    <TwitterShareButton url={url}>
+                      <svg
+                        className="h-6 w-6 text-gray-800 dark:text-white"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M22 5.9c-.7.3-1.5.5-2.4.6a4 4 0 0 0 1.8-2.2c-.8.5-1.6.8-2.6 1a4.1 4.1 0 0 0-6.7 1.2 4 4 0 0 0-.2 2.5 11.7 11.7 0 0 1-8.5-4.3 4 4 0 0 0 1.3 5.4c-.7 0-1.3-.2-1.9-.5a4 4 0 0 0 3.3 4 4.2 4.2 0 0 1-1.9.1 4.1 4.1 0 0 0 3.9 2.8c-1.8 1.3-4 2-6.1 1.7a11.7 11.7 0 0 0 10.7 1A11.5 11.5 0 0 0 20 8.5V8a10 10 0 0 0 2-2.1Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
                     </TwitterShareButton>
-                 </div>
+                  </div>
                 </span>
               </div>
             </div>
@@ -131,9 +151,12 @@ export const PostCard: FC<PostProps> = ({ post }) => {
             {" "}
             <p className=" font-thin"> User 1</p>
             <p className="mb-5 text-left font-title text-2xl hover:text-slate-700 focus:text-slate-200 active:font-semibold">
-            {post?.author?.first_name} {post?.author?.last_name} </p>
+              {post?.author?.first_name} {post?.author?.last_name}{" "}
+            </p>
             <p className="overflow-hidden truncate">{post?.author?.about}</p>
-            <a className=" font-thin" href={post?.author?.email}>Contact this author</a>
+            <a className=" font-thin" href={post?.author?.email}>
+              Contact this author
+            </a>
           </div>
         </div>
         <div className="col-span-1 h-[18.75rem]"></div>
--- src/pages/auth/PostPage.tsx
+++ src/pages/auth/PostPage.tsx
@@ -1,38 +1,36 @@
 import {FC, useEffect, useState} from "react";
 import {Layout} from "@/layout";
 import {PostCard} from "@/features/post/Post";
-import { useParams } from "react-router-dom";
-import { PostProvider } from "@/services/api";
-import { useToast } from "@/hooks";
-import { Post } from "@/services/api/gen";
+import {useParams} from "react-router-dom";
+import {PostProvider} from "@/services/api";
+import {useToast} from "@/hooks";
+import {Post} from "@/services/api/gen";
 
 export const PostPage: FC = () => {
-  const [post, setPost] = useState<Post>()
-  
-  const toast = useToast()
-  const { id } = useParams();
+  const [post, setPost] = useState<Post>();
 
+  const toast = useToast();
+  const {id} = useParams();
 
-  useEffect(()=>{
-    const fetchPost = async ()=>{
+  useEffect(() => {
+    const fetchPost = async () => {
       try {
         const data = await PostProvider.getById(id);
-        setPost(data)
-
+        setPost(data);
       } catch (_e) {
         toast({
           variant: "destructive",
           message: "Unable to fetch post.",
-        })
+        });
       }
-    }
+    };
     if (id) {
-      void fetchPost()
+      void fetchPost();
     }
-  }, [id])
+  }, [id]);
   return (
     <Layout>
-      <PostCard post={post}/>
+      <PostCard post={post} />
     </Layout>
   );
 };
