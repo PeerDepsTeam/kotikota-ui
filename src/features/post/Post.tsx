974324f3808db94e823c45ca6da151ba1e067f10
Karenathi <hei.karenandriantasy@gmail.com>
2024-02-10 03:44:56 +0300

    fix: code style

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -18,8 +18,7 @@ export const Post: FC = () => {
         data-testid="post-title"
         className="mb-4 mt-40 flex w-full justify-center p-11 text-center md:mt-20"
       >
-        
-        <p className=" font-title text-6xl mt-16 "> Post Title</p>
+        <p className=" mt-16 font-title text-6xl "> Post Title</p>
         {/* <p className="font-optical-sizing-auto normal font-title text-6xl font-bold">
           {post.title}
         </p> */}
@@ -91,10 +90,20 @@ export const Post: FC = () => {
 
               <div className="flex items-center gap-8">
                 <Button className=" ml-[42rem]">Donate</Button>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
-</svg>
-
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
+                  />
+                </svg>
               </div>
             </div>
 
