bae5ecfd5c340ada0de55eb074008f1037800092
NyH1409 <hei.nyhasina@gmail.com>
2024-02-10 06:41:37 +0300

    feat: share button

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -1,11 +1,10 @@
 import {FC} from "react";
-import {Link} from "react-router-dom";
 import {Icon} from "@iconify/react";
 import {Layout} from "@/layout";
 import {Icons} from "@/components/common/icons";
-import Share from "@/assets/share.svg";
 import {Button} from "@/components/common/button";
 import pic from "@/assets/profile.jpg";
+import { FacebookShareButton, TwitterShareButton } from "react-share";
 
 // export interface PostProps {
 //   post: PostType;
@@ -65,7 +64,7 @@ export const Post: FC = () => {
               />
             </div>
 
-            <div className=" flex">
+            <div className=" flex justify-between">
               <div className="flex items-stretch justify-self-center">
                 <Button
                   // isLoading={isLoading}
@@ -88,22 +87,24 @@ export const Post: FC = () => {
                 </Button>
               </div>
 
-              <div className="flex items-center gap-8">
-                <Button className=" ml-[42rem]">Donate</Button>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="h-6 w-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
-                  />
-                </svg>
+              <div className="flex justify-between ">
+                <Button>Donate</Button>
+                <span className="flex justify-between">
+                 <div className="mx-2 rounded-[8px] p-2 flex items-center">
+                      <FacebookShareButton  url="http://localhost:5173/posts">
+                        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                          <path fill-rule="evenodd" d="M13.1 6H15V3h-1.9A4.1 4.1 0 0 0 9 7.1V9H7v3h2v10h3V12h2l.6-3H12V6.6a.6.6 0 0 1 .6-.6h.5Z" clip-rule="evenodd"/>
+                        </svg>
+                      </FacebookShareButton>
+                 </div>
+                 <div className="rounded-[8px] p-2 flex items-center">
+                  <TwitterShareButton  url="http://localhost:5173/posts">
+                      <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M22 5.9c-.7.3-1.5.5-2.4.6a4 4 0 0 0 1.8-2.2c-.8.5-1.6.8-2.6 1a4.1 4.1 0 0 0-6.7 1.2 4 4 0 0 0-.2 2.5 11.7 11.7 0 0 1-8.5-4.3 4 4 0 0 0 1.3 5.4c-.7 0-1.3-.2-1.9-.5a4 4 0 0 0 3.3 4 4.2 4.2 0 0 1-1.9.1 4.1 4.1 0 0 0 3.9 2.8c-1.8 1.3-4 2-6.1 1.7a11.7 11.7 0 0 0 10.7 1A11.5 11.5 0 0 0 20 8.5V8a10 10 0 0 0 2-2.1Z" clip-rule="evenodd"/>
+                      </svg>  
+                    </TwitterShareButton>
+                 </div>
+                </span>
               </div>
             </div>
 
