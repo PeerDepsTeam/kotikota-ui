8ae429e21b604379bde2e5f6d738c25412499cb9
Ylan <hei.nalisoa@gmail.com>
2024-02-10 12:57:31 +0300

    feat: login page

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -6,7 +6,7 @@ export const LoginPage: FC = () => {
   // it may also have some layout-ing but at the end, It'll always use its root from the corresponding feature
   return (
     <Layout header={<NavBar page="sign_in" />}>
-      <div className="mx-[2.8rem] h-full pt-[3.8rem] md:mx-[11rem]">
+      <div className="h-full pt-[3.8rem] md:mx-[18rem] mx-auto my-0 items-center justify-center">
         <Login />
       </div>
     </Layout>
