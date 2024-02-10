a558718848424dc31756c9bd2da68db8857bcfa2
Ylan <hei.nalisoa@gmail.com>
2024-02-10 06:21:29 +0300

    feat: profile page (#11)
    * feat: route to profile
    
    * feat: avatar image if user photo null
    
    * feat: component profile
    
    * feat: profile page
    
    * reformat: run prettier

---
+++ src/pages/profile/ProfilePage.tsx
@@ -0,0 +1,29 @@
+import Profile from "@/features/auth/components/Profile";
+import {Layout} from "@/layout";
+import {User, UserSexEnum} from "@/services/api/gen";
+import {FC} from "react";
+import Avatar from "@/assets/images/avatar.png";
+
+export const ProfilePage: FC = () => {
+  const user: User = {
+    id: "123456",
+    last_name: "Doe",
+    first_name: "John",
+    username: "john.doe",
+    birth_date: new Date("1990-01-01"),
+    email: "john.doe@example.com",
+    photo: Avatar,
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    about:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    sex: UserSexEnum.M,
+  };
+
+  return (
+    <Layout>
+      <div className="mx-[2.8rem] h-full pt-[3.8rem] md:mx-[11rem]">
+        <Profile user={user} />
+      </div>
+    </Layout>
+  );
+};
