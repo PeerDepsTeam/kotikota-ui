01944325370335e889acf53df5982b47efa9a326
Yume <hei.hajatiana@gmail.com>
2024-02-10 05:55:12 +0300

    chore: implement providers

--- src/services/api/provider/health_provider.ts
+++ src/services/api/provider/health_provider.ts
@@ -1,9 +1,11 @@
+import {healthApi} from "@/services/api";
+
 export interface HealthProvider {
   ping(): Promise<string>;
 }
 
 export const HealthProvider: HealthProvider = {
   async ping(): Promise<string> {
-    return "";
+    return (await healthApi().ping()).data;
   },
 };
